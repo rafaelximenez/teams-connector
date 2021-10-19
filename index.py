417950ef8171ba9d7ff0c334c6beb4adb0ed6488
Rafael Ximenez <rximenez97@gmail.com>
2021-10-19 15:16:18 -0300

    create teams connection

--- index.py
+++ index.py
@@ -1,18 +1,31 @@
 from datetime import datetime
 from faker import Faker
 
+from teams import Teams
+
 fake = Faker()
+teams = Teams('url')
 
 def __get_period():
     #now=fake.date_time()
     now=datetime.now()
 
     time=int(f'{now.hour}{str(now.minute).zfill(2)}')
-    if time > 0000 and time <= 1200: return 'dia'
-    elif time > 1200 and time <= 1800: return 'tarde'
-    else: return 'noite'
+    if time > 0000 and time <= 1200: return 'Bom dia'
+    elif time > 1200 and time <= 1800: return 'Boa tarde'
+    else: return 'Boa noite'
 
+def calculateRangeDate(value, column1, column2):
+    if 'nat' in [str(value[column1]).lower(), str(value[column2]).lower()] :
+        return 0
 
-for i in range(20):
-    period = __get_period()
-    input('---------------------')
+    d2 = datetime.strptime(str(value[column1]), '%Y-%m-%d %H:%M:%S')
+    d1 = datetime.strptime(str(value[column2]), '%Y-%m-%d %H:%M:%S')
+    interval = abs((d2 - d1).days)
+
+    return interval
+
+period = __get_period()
+
+message = f'{period}, tudo bem?'
+teams.send_message(message)
---
+++ teams.py
@@ -0,0 +1,10 @@
+from pyteams import connectorcard
+
+class Teams:
+    def __init__(self, url_connect):
+        self.url_connect = url_connect
+        
+    def send_message(self, text):
+        channel = connectorcard(self.url_connect)
+        channel.text(text)
+        channel.save()
